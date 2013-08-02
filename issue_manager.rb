f5c387fb357d56c64960c2b3cd0fe06c7628ec2a
Bronagh Sorota <bsorota@redhat.com>
2013-08-02 10:39:19 -0400

    Altered the load_permitted _labels method. Added a client method

--- issue_manager.rb
+++ issue_manager.rb
@@ -17,20 +17,34 @@ class IssueManager
 
 
   def initialize
-    @client = Octokit::Client.new(:login => "xxxxxx", :password => "xxxxxx", :auto_traversal => true)
     @timestamps = load_yaml_file
     if !@timestamps
       @timestamps = Hash.new(0)
     end
-    @permitted_labels = {}
     load_permitted_labels("MANAGEIQ/sandbox")
   end
 
+  def get_notifications
+    puts "get_notifications"
+    notifications = client.repository_notifications("ManageIQ/sandbox", "all" => false)
+    notifications.each do |notification|
+      process_notification(notification)
+    end
+  end
+
+  private
+
+  attr_accessor :permitted_labels
+
+  def client
+    @client ||= Octokit::Client.new(:login => "xxxxxx", :password => "xxxxxx", :auto_traversal => true)
+  end
 
   def load_permitted_labels(repo)
-    labels = @client.labels(repo)
+    permitted_labels ||= {}
+    labels = client.labels(repo)
     labels.each do |label|
-      @permitted_labels[label.name] = 0
+      permitted_labels[label.name] = 0
     end
   end
 
@@ -41,28 +55,25 @@ class IssueManager
     puts "State:\t #{issue.state}"
   end
 
-
   def add_label(issue_id, labels)
-    @client.add_labels_to_an_issue("ManageIQ/sandbox", issue_id, labels)
+    client.add_labels_to_an_issue("ManageIQ/sandbox", issue_id, labels)
   end
 
   def remove_label(issue_id, label)
-    @client.remove_label("ManageIQ/sandbox", issueID, label)
+    client.remove_label("ManageIQ/sandbox", issueID, label)
   end
 
   def get_issue_comments(issue_id)
-    @client.issue_comments("ManageIQ/sandbox", issue_id)
+    client.issue_comments("ManageIQ/sandbox", issue_id)
   end
 
   def find_issue(repository, issue_id)
-    @client.issue("ManageIQ/#{repository}", issue_id)
+    client.issue("ManageIQ/#{repository}", issue_id)
   end
 
   def print_notification(notification)
     puts "Notification repo: #{notification.repository.name}"
     puts "Notification subject title: #{notification.subject.title}"
-    puts "#{notification.subject.url}"
-    puts "#{notification.subject.latest_comment_url}"
   end
 
   def print_comment(comment)
@@ -70,23 +81,16 @@ class IssueManager
     puts "\tComment added at: #{comment.updated_at}\n"
   end
 
-  def get_notifications
-    notifications = @client.repository_notifications("ManageIQ/sandbox", "all" => false)
-    notifications.each do |notification|
-      process_notification(notification)
-    end
-  end
-
   def extract_comment_id(notification)
-    notification.subject.latest_comment_url.match(/[0-9]+\Z/)
+    notification.subject.latest_comment_url.match(/[0-9]+\Z/).to_s
   end
 
   def extract_issue_id(notification)
-    notification.subject.url.match(/[0-9]+\Z/)
+    notification.subject.url.match(/[0-9]+\Z/).to_s
   end
 
   def extract_thread_id(notification)
-    notification.url.match(/[0-9]+\Z/)
+    notification.url.match(/[0-9]+\Z/).to_s
   end
 
   def make_repo_name(notification)
@@ -94,12 +98,11 @@ class IssueManager
   end
 
   def process_notification(notification)
-    #print_notification(notification)
     repo        = make_repo_name(notification)
     thread_id   = extract_thread_id(notification)
     issue_id    = extract_issue_id(notification)
-    issue       = @client.issue(repo, issue_id)   
-    comments    = @client.issue_comments(repo, issue_id)
+    issue       = client.issue(repo, issue_id)   
+    comments    = client.issue_comments(repo, issue_id)
     comments.each do |comment|
       process_comment(comment, issue, repo) 
     end
@@ -113,7 +116,9 @@ class IssueManager
     # than the one in the hash/yaml for this issue 
 
   def process_comment(comment, issue, repo)
-    last_comment_timestamp = @timestamps[issue.number] || 0   
+    print_comment(comment)
+    last_comment_timestamp = @timestamps[issue.number] || 0  
+
     if last_comment_timestamp != 0 && last_comment_timestamp >= comment.updated_at
       return
     end
@@ -123,12 +128,9 @@ class IssueManager
 
     add_and_yaml_timestamps(issue.number, comment.updated_at)       
 
-    match = parse_command_value(comment)
-    return if !match 
-    
-
-    command = match[0].match(/@cfme-bot\s/).post_match
-    command.strip!
+    match = comment.body.match(/^@cfme-bot\s+([-@a-z0-9_]+)\s+/i)
+    return unless match
+    command = match.captures.first
     command_value = match.post_match
 
     method_name = COMMANDS[command]
@@ -136,7 +138,7 @@ class IssueManager
 
       # How to distinguish between the a typo in a command that needs reported back
       # to the user and just a regular comment that starts with @cfme-bot...?
-      # @client.add_comment(repo, issue.id, "invalid command #{command}, ignoring.")
+      # client.add_comment(repo, issue.id, "invalid command #{command}, ignoring.")
 
       return
     else
@@ -147,7 +149,7 @@ class IssueManager
 
 
   def parse_command_value(comment)
-    return comment.body.match(/@cfme-bot\s[@a-z\-\_]+\s/)
+    
   end
 
   def assign_to_issue(repo, issue, assign_to_user)
@@ -159,7 +161,7 @@ class IssueManager
     # assignee and ignore it if it is not set to redhat.
     
     begin
-      user = @client.user(assign_to_user)
+      user = client.user(assign_to_user)
     rescue Octokit::NotFound
        add_assignee_comment(repo, issue, assign_to_user)
        return
@@ -174,20 +176,19 @@ class IssueManager
       add_assignee_comment(repo, issue, assign_to_user)
       return
     end
-    @client.update_issue(repo, issue.number, issue.title, issue.body, "assignee" => assign_to_user)
+    client.update_issue(repo, issue.number, issue.title, issue.body, "assignee" => assign_to_user)
   end
 
-def remove_labels_from_issue(repo, issue, command_value)
+  def remove_labels_from_issue(repo, issue, command_value)
     message             = "Cannot remove the following label(s) because they are not recognized: "
     invalid_labels_bool = false
     valid_labels        = Array.new
     labels_array        = split(command_value)
 
     labels_array.each do |label|
-      label.rstrip!
-      label.lstrip!
+      
       begin
-        label = @client.label(repo, label)
+        label = client.label(repo, label.strip)
       rescue Octokit::NotFound
         message << " " << label << ","
         invalid_labels_bool = true    
@@ -197,55 +198,42 @@ def remove_labels_from_issue(repo, issue, command_value)
     end
 
     if invalid_labels_bool
-      @client.add_comment(repo, issue.number, message.chomp(','))
+      client.add_comment(repo, issue.number, message.chomp(','))
     end
 
     valid_labels.each do |valid_label|
-      @client.remove_label(repo, issue.number, valid_label.name)  
+      client.remove_label(repo, issue.number, valid_label.name)  
     end
-end
-
-
-
-  # def remove_labels_from_issue(repo, issue, command_value)
-  #   labels_array = split(command_value)
-  #   labels_array.each do |label|
-  #     begin
-  #       label = @client.label(repo, label).name
-  #     rescue Octokit::NotFound
-  #       message = "Cannot find label #{label}"
-  #       @client.add_comment(repo, issue.number, message)
-  #       next
-  #     end
-  #     @client.remove_label(repo, issue.number, label)
-  #   end
-  # end
-
-  def add_assignee_comment(repo, issue, assign_to_user)
-    message = "Assignee #{assign_to_user} is an invalid user."
-    @client.add_comment(repo, issue.number, message)
   end
 
 
-  def check_user_organization()
-    members_array = @client.organization_members("ManageIQ")
-      members_array.each do |members_hash|
-        members_hash.each do |member|
-          puts "HASH: #{member[1]}"
-        end
-      end
+  def add_assignee_comment(repo, issue, assign_to_user)
+    message = "Assignee #{assign_to_user} is an invalid user."
+    client.add_comment(repo, issue.number, message)
+  end
+
+
+  def check_user_organization(user)
+    oragnization_members.include?(user)
+  end
+
+  def oragnization_members
+    members_array = client.organization_members("ManageIQ")
+    member_names = members_array.collect do |members_hash|
+      members_hash["username"]
+    end
+    Set.new(member_names)
   end
 
   def add_labels_to_an_issue(repo, issue, command_value)
     message = "Applying the following label(s) is not permitted:"
 
     new_labels        = split(command_value)
-    puts "new labels #{new_labels}"
     new_labels_length = new_labels.length
     allowed_labels    = Array.new
 
     new_labels.each do |new_label|
-      if !@permitted_labels[new_label]
+      if !permitted_labels[new_label]
         message << " " << new_label << ","
         next
       else
@@ -254,19 +242,17 @@ end
     end
 
     if new_labels.length - allowed_labels.length > 0
-      @client.add_comment(repo, issue.number, message.chomp(','))
+      client.add_comment(repo, issue.number, message.chomp(','))
     end
 
     if !allowed_labels.empty?
-      @client.add_labels_to_an_issue(repo, issue.number, allowed_labels)
+      client.add_labels_to_an_issue(repo, issue.number, allowed_labels)
     end
   end
 
   def mark_thread_as_read(thread_id)
-    @client.mark_thread_as_read(thread_id, "read" => false )
-
+    client.mark_thread_as_read(thread_id, "read" => false )
   end
-
 
   def split(labels)
     labels.split(", ")
@@ -289,13 +275,15 @@ end
     end 
   end
 end
- 
-issue_manager = IssueManager.new
-loop {
-  begin
-    issue_manager.get_notifications
-  rescue =>err
-    puts "ERROR: #{err.message} \n #{err.backtrace} \n"
-  end
-  sleep(SLEEPTIME)
-}
+
+if __FILE__ == $0
+  issue_manager = IssueManager.new
+  loop {
+    begin
+      issue_manager.get_notifications
+    rescue =>err
+      puts "ERROR: #{err.message} \n #{err.backtrace} \n"
+    end
+    sleep(SLEEPTIME)
+  }
+end
