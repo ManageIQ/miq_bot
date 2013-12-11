070062005f1ad92ddd81b9aac1883a6bf1adf242
Jason Frey <jfrey@redhat.com>
2013-12-11 15:36:34 -0500

    Create Rails app to interface with cfme_bot.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfmeBot::Application.config.secret_key_base = '735e70f34828776d42672bd80562c3f65885957d4e69f78aba26f998a1882003936dbaab933043c41afdcbc9c8dbf0aa625bacc4cab18e7b17452d40e9545ecf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CfmeBot::Application.config.session_store :cookie_store, key: '_cfme_bot_session'
