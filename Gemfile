380822a3a256cb6a4e0c6482c37d4f310d528166
Jason Frey <jfrey@redhat.com>
2013-12-11 15:53:50 -0500

    Add sidekiq and mount UI to Rails app under /sidekiq

--- Gemfile
+++ Gemfile
@@ -43,3 +43,7 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+gem 'sidekiq'
+gem 'sinatra', require: false
+gem 'slim'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 CfmeBot::Application.routes.draw do
+  mount Sidekiq::Web, at: "/sidekiq"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
