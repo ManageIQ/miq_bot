4c3ea5d0b5bc32265485c702e3550268f319c88c
Jason Frey <jfrey@redhat.com>
2014-02-03 22:00:31 -0500

    Change UI to sort by repo name then branch name.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
   def index
-    @repos = CommitMonitorRepo.includes(:branches)
+    @branches = CommitMonitorBranch.includes(:repo).sort_by { |b| [b.repo.name, b.name] }
   end
 end
