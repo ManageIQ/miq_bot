20fd193a0b6defd53ffb304f8406fd465894d8fd
Jason Frey <jfrey@redhat.com>
2014-03-10 16:45:41 -0400

    Add helper method for adding a new github based repo.

--- app/models/commit_monitor_repo.rb
+++ app/models/commit_monitor_repo.rb
@@ -4,6 +4,27 @@ class CommitMonitorRepo < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   validates :path, :presence => true, :uniqueness => true
 
+  def self.create_from_github!(upstream_user, name, path)
+    GitService.call(path) do |git|
+      git.checkout("master")
+      git.pull
+
+      repo = self.create!(
+        :upstream_user => upstream_user,
+        :name          => name,
+        :path          => File.expand_path(path)
+      )
+
+      repo.branches.create!(
+        :name        => "master",
+        :commit_uri  => CommitMonitorBranch.github_commit_uri(upstream_user, name),
+        :last_commit => git.current_ref
+      )
+
+      repo
+    end
+  end
+
   def path=(val)
     super(File.expand_path(val))
   end
