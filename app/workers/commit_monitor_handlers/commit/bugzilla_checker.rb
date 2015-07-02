e73331e1f865e91d4e4d1753adf6302d2a684024
Nick Carboni <ncarboni@redhat.com>
2015-07-02 15:58:12 -0400

    More rubocop refactoring.
    Moved the code that gets the bug object from ActiveBugzilla into a
    method that expects a block.  This way it can be reused for both types
    of bugzilla checks.

--- app/workers/commit_monitor_handlers/commit/bugzilla_checker.rb
+++ app/workers/commit_monitor_handlers/commit/bugzilla_checker.rb
@@ -43,6 +43,21 @@ module CommitMonitorHandlers
         comment
       end
 
+      def with_bug(bug_id)
+        return unless block_given?
+        MiqToolsServices::Bugzilla.call do
+          output = ActiveBugzilla::Bug.find(:product => product, :id => bug_id)
+          if output.empty?
+            logger.error "Unable to write for bug id #{bug_id}: Not a '#{product}' bug."
+          else
+            bug = output.first
+            yield(bug)
+          end
+        end
+      rescue => err
+        logger.error "Unable to write for bug id #{bug_id}: #{err}"
+      end
+
       def process_commit
         MiqToolsServices::Bugzilla.ids_in_git_commit_message(message).each do |bug_id|
           if @branch.pull_request?
@@ -53,57 +68,44 @@ module CommitMonitorHandlers
         end
       end
 
-      def update_bugzilla_status(bug_id)
-        logger.info "Running bugzilla status update for bug id #{bug_id}"
-        MiqToolsServices::Bugzilla.call do
-          output = ActiveBugzilla::Bug.find(:product => product, :id => bug_id)
-          if output.empty?
-            logger.error "Unable to update status for bug id #{bug_id}: Not a '#{product}' bug."
-          else
-            bug = output.first
-
-            if bug_has_pr_uri_comment?(bug)
-              logger.info "Not commenting on bug #{bug_id} due to duplicate comment."
-            else
-              logger.info "Adding PR comment to bug #{bug_id}."
-              bug.add_comment(@branch.github_pr_uri)
-            end
-
-            if bug.status == "NEW" || bug.status == "ASSIGNED"
-              logger.info "Changing status of bug #{bug_id} to ON_DEV."
-              bug.status = "ON_DEV"
-            else
-              logger.warn "Not changing status of bug #{bug_id} from #{bug.status}."
-              if bug.status != "ON_DEV"
-                bug.add_comment("Detected commit referencing this ticket while ticket status is #{bug.status}.")
-                bug.flags["needinfo"] = "?"
-              end
-            end
-
-            bug.save
-          end
+      def write_to_bugzilla(bug_id)
+        with_bug(bug_id) do |bug|
+          logger.info "Writing to bugzilla for bug id #{bug_id}"
+          bug.add_comment(bugzilla_comment)
+          bug.save
         end
-      rescue => err
-        logger.error "Unable to update status for bug id #{bug_id}: #{err}"
       end
 
-      def write_to_bugzilla(bug_id)
-        log_prefix = "#{self.class.name}##{__method__}"
-        logger.info("#{log_prefix} Updating bug id #{bug_id} in Bugzilla.")
+      def add_pr_comment(bug)
+        if bug_has_pr_uri_comment?(bug)
+          logger.info "Not commenting on bug #{bug.id} due to duplicate comment."
+        else
+          logger.info "Adding PR comment to bug #{bug.id}."
+          bug.add_comment(@branch.github_pr_uri)
+        end
+      end
 
-        MiqToolsServices::Bugzilla.call do
-          output = ActiveBugzilla::Bug.find(:product => product, :id => bug_id)
-          if output.empty?
-            logger.error "#{log_prefix} Unable to write for bug id #{bug_id}: Not a '#{product}' bug."
-          else
-            logger.info "#{log_prefix} Writing to bugzilla for bug id #{bug_id}"
-            bug = output.first
-            bug.add_comment(bugzilla_comment)
-            bug.save
+      def update_bug_status(bug)
+        bug_id = bug.id
+        bug_stat = bug.status
+        if bug_stat == "NEW" || bug_stat == "ASSIGNED"
+          logger.info "Changing status of bug #{bug_id} to ON_DEV."
+          bug.status = "ON_DEV"
+        else
+          logger.warn "Not changing status of bug #{bug_id} from #{bug_stat}."
+          if bug_stat != "ON_DEV"
+            bug.add_comment("Detected commit referencing this ticket while ticket status is #{bug_stat}.")
+            bug.flags["needinfo"] = "?"
           end
         end
-      rescue => err
-        logger.error "#{log_prefix} Unable to write for bug id #{bug_id}: #{err}"
+      end
+
+      def update_bugzilla_status(bug_id)
+        with_bug(bug_id) do |bug|
+          add_pr_comment(bug)
+          update_bug_status(bug)
+          bug.save
+        end
       end
     end
   end
