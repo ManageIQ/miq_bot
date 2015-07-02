25b45f4d48d15ed4400856900af934be6e0a3275
Nick Carboni <ncarboni@redhat.com>
2015-07-02 14:13:39 -0400

    Fixing some rubocop issues

--- app/workers/commit_monitor_handlers/commit/bugzilla_checker.rb
+++ app/workers/commit_monitor_handlers/commit/bugzilla_checker.rb
@@ -1,102 +1,110 @@
-class CommitMonitorHandlers::Commit::BugzillaChecker
-  include Sidekiq::Worker
-  sidekiq_options :queue => :miq_bot
+module CommitMonitorHandlers
+  module Commit
+    class BugzillaChecker
+      include Sidekiq::Worker
+      sidekiq_options :queue => :miq_bot
 
-  def self.handled_branch_modes
-    [:regular, :pr]
-  end
+      def self.handled_branch_modes
+        [:regular, :pr]
+      end
 
-  attr_reader :branch, :commit, :message
+      attr_reader :branch, :commit, :message
 
-  def perform(branch_id, commit, commit_details)
-    logger.info("Performing bugzilla check on branch #{branch_id}")
-    @branch  = CommitMonitorBranch.where(:id => branch_id).first
-    @commit  = commit
-    @message = commit_details["message"]
+      def perform(branch_id, commit, commit_details)
+        logger.info("Performing bugzilla check on branch #{branch_id}")
+        @branch  = CommitMonitorBranch.where(:id => branch_id).first
+        @commit  = commit
+        @message = commit_details["message"]
 
-    if @branch.nil?
-      logger.info("Branch #{branch_id} no longer exists.  Skipping.")
-      return
-    end
+        if @branch.nil?
+          logger.info("Branch #{branch_id} no longer exists.  Skipping.")
+          return
+        end
 
-    process_commit
-  end
+        process_commit
+      end
 
-  private
+      private
 
-  def product
-    Settings.commit_monitor.bugzilla_product
-  end
+      def product
+        Settings.commit_monitor.bugzilla_product
+      end
 
-  def bug_has_pr_uri_comment?(bug)
-    bug.comments.any? do |c|
-      c.text.include?(@branch.github_pr_uri)
-    end
-  end
+      def bug_has_pr_uri_comment?(bug)
+        bug.comments.any? do |c|
+          c.text.include?(@branch.github_pr_uri)
+        end
+      end
 
-  def process_commit
-    MiqToolsServices::Bugzilla.ids_in_git_commit_message(message).each do |bug_id|
-      if @branch.pull_request?
-        update_bugzilla_status(bug_id)
-      else
+      def bugzilla_comment
         prefix     = "New commit detected on #{branch.repo.name}/#{branch.name}:"
         commit_uri = branch.commit_uri_to(commit)
         comment    = "#{prefix}\n#{commit_uri}\n\n#{message}"
-        write_to_bugzilla(bug_id, comment)
+        comment
       end
-    end
-  end
 
-  def update_bugzilla_status(bug_id)
-    logger.info "Running bugzilla status update for bug id #{bug_id}"
-    MiqToolsServices::Bugzilla.call do |bz|
-      output = ActiveBugzilla::Bug.find(:product => product, :id => bug_id)
-      if output.empty?
-        logger.error "Unable to update status for bug id #{bug_id}: Not a '#{product}' bug."
-      else
-        bug = output.first
-
-        if bug_has_pr_uri_comment?(bug)
-          logger.info "Not commenting on bug #{bug_id} due to duplicate comment."
-        else
-          logger.info "Adding PR comment to bug #{bug_id}."
-          bug.add_comment(@branch.github_pr_uri)
-        end
-
-        if bug.status == "NEW" || bug.status == "ASSIGNED"
-          logger.info "Changing status of bug #{bug_id} to ON_DEV."
-          bug.status = "ON_DEV"
-        else
-          logger.warn "Not changing status of bug #{bug_id} from #{bug.status}."
-          if bug.status != "ON_DEV"
-            bug.add_comment("Detected commit referencing this ticket while ticket status is #{bug.status}.")
-            bug.flags["needinfo"] = "?"
+      def process_commit
+        MiqToolsServices::Bugzilla.ids_in_git_commit_message(message).each do |bug_id|
+          if @branch.pull_request?
+            update_bugzilla_status(bug_id)
+          else
+            write_to_bugzilla(bug_id)
           end
         end
+      end
 
-        bug.save
+      def update_bugzilla_status(bug_id)
+        logger.info "Running bugzilla status update for bug id #{bug_id}"
+        MiqToolsServices::Bugzilla.call do
+          output = ActiveBugzilla::Bug.find(:product => product, :id => bug_id)
+          if output.empty?
+            logger.error "Unable to update status for bug id #{bug_id}: Not a '#{product}' bug."
+          else
+            bug = output.first
+
+            if bug_has_pr_uri_comment?(bug)
+              logger.info "Not commenting on bug #{bug_id} due to duplicate comment."
+            else
+              logger.info "Adding PR comment to bug #{bug_id}."
+              bug.add_comment(@branch.github_pr_uri)
+            end
+
+            if bug.status == "NEW" || bug.status == "ASSIGNED"
+              logger.info "Changing status of bug #{bug_id} to ON_DEV."
+              bug.status = "ON_DEV"
+            else
+              logger.warn "Not changing status of bug #{bug_id} from #{bug.status}."
+              if bug.status != "ON_DEV"
+                bug.add_comment("Detected commit referencing this ticket while ticket status is #{bug.status}.")
+                bug.flags["needinfo"] = "?"
+              end
+            end
+
+            bug.save
+          end
+        end
+      rescue => err
+        logger.error "Unable to update status for bug id #{bug_id}: #{err}"
+      end
+
+      def write_to_bugzilla(bug_id)
+        log_prefix = "#{self.class.name}##{__method__}"
+        logger.info("#{log_prefix} Updating bug id #{bug_id} in Bugzilla.")
+
+        MiqToolsServices::Bugzilla.call do
+          output = ActiveBugzilla::Bug.find(:product => product, :id => bug_id)
+          if output.empty?
+            logger.error "#{log_prefix} Unable to write for bug id #{bug_id}: Not a '#{product}' bug."
+          else
+            logger.info "#{log_prefix} Writing to bugzilla for bug id #{bug_id}"
+            bug = output.first
+            bug.add_comment(bugzilla_comment)
+            bug.save
+          end
+        end
+      rescue => err
+        logger.error "#{log_prefix} Unable to write for bug id #{bug_id}: #{err}"
       end
     end
-  rescue => err
-    logger.error "Unable to update status for bug id #{bug_id}: #{err}"
-  end
-
-  def write_to_bugzilla(bug_id, comment)
-    log_prefix = "#{self.class.name}##{__method__}"
-    logger.info("#{log_prefix} Updating bug id #{bug_id} in Bugzilla.")
-
-    MiqToolsServices::Bugzilla.call do |bz|
-      output = ActiveBugzilla::Bug.find(:product => product, :id => bug_id)
-      if output.empty?
-        logger.error "#{log_prefix} Unable to write for bug id #{bug_id}: Not a '#{product}' bug."
-      else
-        logger.info "#{log_prefix} Writing to bugzilla for bug id #{bug_id}"
-        bug = output.first
-        bug.add_comment(comment)
-        bug.save
-      end
-    end
-  rescue => err
-    logger.error "#{log_prefix} Unable to write for bug id #{bug_id}: #{err}"
   end
 end
