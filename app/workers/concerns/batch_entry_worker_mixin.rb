29e0a44ec977794083bf0c8f36485b2f5f64a5f1
Jason Frey <jfrey@redhat.com>
2015-08-15 02:17:51 -0400

    Add BatchEntryWorkerMixin for common methods.

---
+++ app/workers/concerns/batch_entry_worker_mixin.rb
@@ -0,0 +1,24 @@
+module BatchEntryWorkerMixin
+  attr_reader :batch_entry
+
+  def find_batch_entry(batch_entry_id)
+    @batch_entry = BatchEntry.where(:id => batch_entry_id).first
+
+    if @batch_entry.nil?
+      logger.warn("BatchEntry #{batch_entry_id} no longer exists.  Skipping.")
+      return false
+    end
+
+    true
+  end
+
+  def batch_job
+    batch_entry.job
+  end
+
+  def complete_batch_entry(updates = {})
+    updates = updates.reverse_merge(:state => "succeeded")
+    batch_entry.update_attributes!(updates)
+    batch_entry.check_job_complete
+  end
+end
