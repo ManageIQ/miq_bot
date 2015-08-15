e5e1b8c4503d3a9a58d26da727ec2d032e4142c5
Jason Frey <jfrey@redhat.com>
2015-08-15 02:02:14 -0400

    Add BatchJobWorkerMixin for common methods.

---
+++ app/workers/concerns/batch_job_worker_mixin.rb
@@ -0,0 +1,30 @@
+module BatchJobWorkerMixin
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def perform_batch_async(*args)
+      BatchJob.perform_async(batch_workers, args,
+        :on_complete_class => self,
+        :on_complete_args  => args,
+        :expires_at        => 5.minutes.from_now
+      )
+    end
+  end
+
+  attr_reader :batch_job
+
+  def find_batch_job(batch_job_id)
+    @batch_job = BatchJob.where(:id => batch_job_id).first
+
+    if @batch_job.nil?
+      logger.warn("BatchJob #{batch_job_id} no longer exists.  Skipping.")
+      return false
+    end
+
+    true
+  end
+
+  def complete_batch_job
+    batch_job.destroy
+  end
+end
---
+++ spec/workers/concerns/batch_job_worker_mixin_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe BatchJobWorkerMixin do
+  let(:includer_class) do
+    Class.new do
+      include BatchJobWorkerMixin
+
+      def self.batch_workers
+        @batch_workers ||= [Class.new, Class.new]
+      end
+
+      def logger
+        @logger ||= RSpec::Mocks::Double.new("logger")
+      end
+    end
+  end
+
+  subject   { includer_class.new }
+  let(:job) { BatchJob.create! }
+
+  it ".perform_batch_async" do
+    expect(BatchJob).to receive(:perform_async) do |workers, worker_args, job_attributes|
+      expect(workers).to     eq(includer_class.batch_workers)
+      expect(worker_args).to eq(%w(arg1 arg2))
+
+      expect(job_attributes[:on_complete_class]).to eq(includer_class)
+      expect(job_attributes[:on_complete_args]).to  eq(%w(arg1 arg2))
+      expect(job_attributes[:expires_at]).to        be_kind_of(Time)
+    end
+
+    includer_class.perform_batch_async("arg1", "arg2")
+  end
+
+  describe "#find_batch_job" do
+    it "with an existing job" do
+      expect(subject.find_batch_job(job.id)).to be true
+    end
+
+    it "with a missing job" do
+      expect(subject.logger).to receive(:warn) do |message|
+        expect(message).to match(/no longer exists/)
+      end
+      expect(subject.find_batch_job(-1)).to be false
+    end
+  end
+
+  it "#complete_batch_job" do
+    subject.find_batch_job(job.id)
+
+    subject.complete_batch_job
+
+    expect { job.reload }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+end
