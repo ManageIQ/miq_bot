156d75696f87554809da6d49aad24493230a7712
Martin Hradil <mhradil@redhat.com>
2017-01-11 19:32:29 +0000

    Linter::ESLint & Linter::SCSS - add convert_parsed
    converting from each format to the one used by rubocop
    
    eslint output:
    
    ```
    [
      {
        filePath: "foo/bar",
        messages: [
          {
            ruleId: "baz",
            severity: 1,
            message: "quux",
            line: 4,
            column: 5,
          },
        ],
      },
    ]
    ```
    
    scss output:
    
    ```
    {
      "foo/bar": [
        {
          line: 4,
          column: 5,
          severity: "warning",
          reason: "quux",
          linter: "baz",
        },
      ],
    }
    ```
    
    converted output:
    
    ```
    {
      files: [
        {
          path: "foo/bar",
          offenses: [
            {
              severity: "warning",
              message: "quux",
              location: {
                line: 4,
                column: 5,
              },
              cop_name: "baz",
            },
          ],
        },
      ],
      summary: {
        target_file_count: 1,
        offense_count: 1,
      },
    }
    ```

--- lib/linter/eslint.rb
+++ lib/linter/eslint.rb
@@ -19,5 +19,27 @@ module Linter
         file.end_with? '.js'
       end
     end
+
+    def convert_parsed(array)
+      files = array.map { |f| convert_file(f) }
+
+      {:files   => files,
+       :summary => {:offense_count     => files.reduce(0) { |memo, f| memo + f.offenses.count },
+                    :target_file_count => files.count}}
+    end
+
+    def convert_file(file)
+      {:path     => file[:filePath],
+       :offenses => file[:messages].map { |m| convert_offense(m) }}
+    end
+
+    SEVERITY = ['off', 'warning', 'error']
+    def convert_offense(offense)
+      {:severity => SEVERITY[offense[:severity]],
+       :message  => offense[:message],
+       :location => {:line   => offense[:line],
+                     :column => offense[:column]},
+       :cop_name => offense[:ruleId]}
+    end
   end
 end
--- lib/linter/scss.rb
+++ lib/linter/scss.rb
@@ -17,5 +17,27 @@ module Linter
     def filtered_files(files)
       files.select { |file| file.end_with?(".scss") }
     end
+
+    def convert_parsed(hash)
+      files = hash.map { |path, offenses| convert_file(path, offenses) }
+
+      {:files   => files,
+       :summary => {:offense_count     => files.reduce(0) { |memo, f| memo + f.offenses.count },
+                    :target_file_count => files.count}}
+    end
+
+    def convert_file(path, offenses)
+      {:path     => path,
+       :offenses => offenses.map { |o| convert_offense(o) }}
+    end
+
+    def convert_offense(offense)
+      {:severity => offense[:severity],
+       :message  => offense[:reason],
+       :location => {:line   => offense[:line],
+                     :column => offense[:column]},
+       :cop_name => offense[:linter]}
+    end
+  end
   end
 end
