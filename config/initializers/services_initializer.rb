40d05c8a76910bf312b4111e44b1c00619ea8fd2
Jason Frey <jfrey@redhat.com>
2017-01-20 18:25:01 -0500

    Add support for Github rate limit logging

--- config/initializers/services_initializer.rb
+++ config/initializers/services_initializer.rb
@@ -1,6 +1,7 @@
 unless Rails.env.test?
   BugzillaService.credentials = Settings.bugzilla_credentials
   GithubService.credentials   = Settings.github_credentials
+  PivotalService.credentials  = Settings.pivotal_credentials
 
-  PivotalService.credentials = Settings.pivotal_credentials
+  GithubService.logger = Sidekiq::Logging.logger
 end
