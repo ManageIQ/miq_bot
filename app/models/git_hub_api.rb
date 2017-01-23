bd11940b7100efd0235410c0beddd077b6d2028a
Jason Frey <jfrey@redhat.com>
2017-01-23 17:46:43 -0500

    Change the GitHubApi to better log ratelimits to the sidekiq logger.

--- app/models/git_hub_api.rb
+++ app/models/git_hub_api.rb
@@ -9,20 +9,21 @@ module GitHubApi
   end
 
   def self.execute(client, cmd, *args)
-    rate_limit_remaining = client.rate_limit.remaining
-    logger.debug("Executing #{cmd} #{args.inspect}...api calls remaining #{rate_limit_remaining}")
+    limit_before = client.rate_limit.remaining
+    logger.debug("Executing #{cmd} #{args.inspect}...api calls remaining #{limit_before}")
     val = nil
     t = Benchmark.realtime { val = client.send(cmd, *args) }
-    logger.debug("Executing #{cmd} #{args.inspect}...Completed in #{t}s and used #{rate_limit_remaining - client.rate_limit.remaining} api calls")
+    limit_after = client.rate_limit.remaining
+    logger.info("Executed #{cmd} #{args.inspect}...api calls remaining #{limit_after} (in #{"%0.3f" % t}s using #{limit_before - limit_after} calls)")
     val
   rescue => err
-    logger.error("Executing #{cmd} #{args.inspect}...Failed in #{t}s")
+    logger.error("Executed #{cmd} #{args.inspect}...Failed in #{"%0.3f" % t}s")
     logger.error("#{err.class}: #{err}")
     logger.error(err.backtrace.join("\n"))
     raise
   end
 
   def self.logger
-    Rails.logger
+    Sidekiq::Logging.logger
   end
 end
