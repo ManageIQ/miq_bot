1f74e5f1bdb0df4884ef4e8c9877f9001960b159
Chris Arcand <chris@chrisarcand.com>
2017-01-25 13:21:13 -0600

    Very regrettably hack away unusable logging
    See links in code for related issues.
    
    I wrote the helper module mostly because I thought it put InfluxDB in an
    awkward state, but it's just the logging going haywire. So remove that
    and let the client bubble messages.

--- config/initializers/influxdb-rails.rb
+++ config/initializers/influxdb-rails.rb
@@ -3,3 +3,22 @@ InfluxDB::Rails.configure do |config|
   config.influxdb_username = ENV['INFLUX_USERNAME']
   config.influxdb_password = ENV['INFLUX_PASSWORD']
 end
+
+# HACK: InfluxDB's Ruby and Rails clients have some issues with their logging strategy.
+# Until the following Issues are resolved, we turn off the ridiculous amount of logging
+# of background workers by nulling their log method.
+#
+# Logging from the background workers is constant and absolutely impossible to work with
+# in the Rails console...
+# * https://github.com/influxdata/influxdb-ruby/issues/180
+#
+# ...but you can't turn it off from here, cleanly.
+# * https://github.com/influxdata/influxdb-rails/issues/35
+
+InfluxDB::Writer::Async::Worker.include(
+  Module.new {
+    def log(level, message)
+      # NOOP
+    end
+  }
+)
--- lib/github_usage_tracker.rb
+++ lib/github_usage_tracker.rb
@@ -4,7 +4,6 @@ class GithubUsageTracker
   end
 
   def record_datapoint
-    InfluxHelper.ensure_database_exists!
     # TODO Clean this mess up
     client = Octokit::Client.new(login: Settings.github_credentials.username, password: Settings.github_credentials.password)
     influxdb.write_point('api_requests_remaining', :values => { :count => client.rate_limit!.remaining })
