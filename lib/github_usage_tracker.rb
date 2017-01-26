0540ee1d85287876a028708abbfa3fa4013ceab3
Chris Arcand <chris@chrisarcand.com>
2017-01-26 14:21:15 -0600

    Return average in 1 minute increments
    Because we're now recording observations on every Github request, we
    don't want to show every single datapoint on the chart. Group by and
    average the calls in a 1 minute interval.
    
    Data is displayed only for the last 12 hours because anything larger
    just isn't that helpful to look at (checked with enormous sample
    dataset) We really need to do Grafana if we want to dig in deep yet
    still see large trends at the same time.

--- lib/github_usage_tracker.rb
+++ lib/github_usage_tracker.rb
@@ -7,8 +7,22 @@ class GithubUsageTracker
     new.rate_limit_measurements
   end
 
+  ##
+  # Returns rate limit measurements over the last 12 hours grouped by the
+  # average of 1 minute intervals.
+  #
   def rate_limit_measurements
-    influxdb.query('SELECT * FROM rate_limit')
+    current_time = DateTime.now
+    from_time    = current_time - 12.hours
+    query = <<-eos
+      SELECT MEAN(requests_remaining)
+      FROM rate_limit
+      WHERE time > '#{from_time.rfc3339}'
+      AND time <= '#{current_time.rfc3339}'
+      GROUP BY time(1m)
+    eos
+
+    influxdb.query(query)
   end
 
   def record_datapoint(requests_remaining:, timestamp: nil)
