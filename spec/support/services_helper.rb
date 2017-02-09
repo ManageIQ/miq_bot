1a0aab78648f75a57f4b9d6f234173313f3459db
Chris Arcand <chris@chrisarcand.com>
2017-02-09 16:32:50 -0600

    Remove unused stub_github_service

--- spec/support/services_helper.rb
+++ spec/support/services_helper.rb
@@ -1,9 +1,3 @@
-def stub_github_service
-  double("Github service").tap do |github|
-    allow(GithubService).to receive(:call).and_yield(github)
-  end
-end
-
 def stub_github_prs(*prs)
   prs.flatten!
   prs.collect! { |i| double("Github PR #{i}", :number => i) } if prs.first.kind_of?(Numeric)
