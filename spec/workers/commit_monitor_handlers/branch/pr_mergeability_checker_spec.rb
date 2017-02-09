52658e8cc6630617a1a50ca15f7734e95298699b
Chris Arcand <chris@chrisarcand.com>
2017-02-08 22:35:26 -0600

    Move PR Mergeability Checker to new Github service

--- spec/workers/commit_monitor_handlers/branch/pr_mergeability_checker_spec.rb
+++ spec/workers/commit_monitor_handlers/branch/pr_mergeability_checker_spec.rb
@@ -5,16 +5,17 @@ describe CommitMonitorHandlers::Branch::PrMergeabilityChecker do
     stub_sidekiq_logger
   end
 
-  let(:github_service) { stub_github_service }
   let(:pr_branch) { create(:pr_branch, :name => 'prs/1/head', :mergeable => true) }
+  let(:repo_name) { pr_branch.repo.name }
 
   context 'when PR was mergeable and becomes unmergeable' do
     it 'comments on the PR' do
       git_service = instance_double('GitService::Branch', :mergeable? => false)
       allow(GitService::Branch).to receive(:new) { git_service }
-      allow(github_service).to receive(:add_issue_labels)
+      allow(NewGithubService).to receive(:add_labels_to_an_issue)
 
-      expect(github_service).to receive(:create_issue_comments)
+      expect(NewGithubService).to receive(:add_comment)
+        .with(repo_name, 1, a_string_including("not mergeable"))
 
       described_class.new.perform(pr_branch.id)
     end
@@ -22,28 +23,27 @@ describe CommitMonitorHandlers::Branch::PrMergeabilityChecker do
     it "adds an 'unmergeable' label to the PR" do
       git_service = instance_double('GitService::Branch', :mergeable? => false)
       allow(GitService::Branch).to receive(:new) { git_service }
-      allow(github_service).to receive(:create_issue_comments)
+      allow(NewGithubService).to receive(:add_comment)
 
-      expect(github_service).to receive(:add_issue_labels).with(1, 'unmergeable')
+      expect(NewGithubService).to receive(:add_labels_to_an_issue)
+        .with(repo_name, 1, ['unmergeable'])
 
       described_class.new.perform(pr_branch.id)
     end
   end
 
   context 'when PR was unmergeable and becomes mergeable' do
-    let(:unmergeable_pr_branch) { create(:pr_branch, :name => 'prs/1/head', :mergeable => false) }
+    let(:pr_branch) { create(:pr_branch, :name => 'prs/1/head', :mergeable => false) }
 
     it "removes an 'unmergeable' label from the PR" do
       git_service = instance_double('GitService::Branch', :mergeable? => true)
       allow(GitService::Branch).to receive(:new) { git_service }
-      allow(github_service).to receive(:create_issue_comments)
+      allow(NewGithubService).to receive(:add_comment)
 
-      allow(github_service).to receive(:user) { 'ManageIQ' }
-      allow(github_service).to receive(:repo) { 'miq_bot' }
-      expect(github_service).to receive_message_chain(:issues, :labels, :remove)
-        .with('ManageIQ', 'miq_bot', 1, :label_name => 'unmergeable')
+      expect(NewGithubService).to receive(:remove_label)
+        .with(repo_name, 1, 'unmergeable')
 
-      described_class.new.perform(unmergeable_pr_branch.id)
+      described_class.new.perform(pr_branch.id)
     end
   end
 end
