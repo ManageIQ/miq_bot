258b53cd6bbaa470d2f53666395a20946dc7526b
Jason Frey <jfrey@redhat.com>
2017-02-16 18:02:14 -0500

    Make influxdb actually optional

--- lib/github_usage_tracker.rb
+++ lib/github_usage_tracker.rb
@@ -5,6 +5,10 @@ class GithubUsageTracker
     new.record_datapoint(args)
   end
 
+  def self.configured?
+    @configured ||= Settings.influxdb_credentials.to_hash.delete_blanks.present?
+  end
+
   def self.influxdb
     @influx ||= InfluxDB::Client.new(Settings.influxdb_credentials.database,
                                      :username => Settings.influxdb_credentials.username,
@@ -13,6 +17,8 @@ class GithubUsageTracker
   end
 
   def record_datapoint(requests_remaining:, uri:, timestamp: Time.now)
+    return unless configured?
+
     request_uri = URI.parse(uri).path.chomp("/")
 
     values = { :tags      => { :bot_version        => MiqBot.version },
@@ -29,15 +35,13 @@ class GithubUsageTracker
 
   private
 
+  delegate :influxdb, :configured?, :to => self
+
   def worker_from_backtrace
     caller.each do |l|
       match = /(?:app\/workers\/)(?:\w+\/)*?(\w+)(?:\.rb\:\d+)/.match(l)
       return match[1] if match && match[1].exclude?("_mixin")
     end
     nil
-  end
-
-  def influxdb
-    self.class.influxdb
   end
 end
