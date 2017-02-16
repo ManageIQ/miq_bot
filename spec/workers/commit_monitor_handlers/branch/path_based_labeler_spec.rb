0d2bc89bdde870efdcca152c7afc3f987098c874
Chris Arcand <chris@chrisarcand.com>
2017-02-15 22:01:49 -0600

    Delete SqlMigrationLabeler :heart_eyes: :fire: :scissors:
    This can now be accomplished by just using the PathBasedLabeler
    with the following pattern: /db\/migrate.*.rb\z/

--- spec/workers/commit_monitor_handlers/branch/path_based_labeler_spec.rb
+++ spec/workers/commit_monitor_handlers/branch/path_based_labeler_spec.rb
@@ -2,7 +2,8 @@ describe CommitMonitorHandlers::Branch::PathBasedLabeler do
   let(:branch)         { create(:pr_branch) }
   let(:git_service)    { double("GitService", :diff => double("RuggedDiff", :new_files => new_files)) }
   let(:settings) do
-    { "path_based_labeler" => { "enabled_repos" => { branch.repo.name => [{ "pattern" => '(Gemfile\z)|(Gemfile\.lock\z)|(\.gemspec\z)', "label" => "gem changes" }] } } }
+    { "path_based_labeler" => { "enabled_repos" => { branch.repo.name => [{ "pattern" => '(Gemfile\z)|(Gemfile\.lock\z)|(\.gemspec\z)', "label" => "gem changes" },
+                                                                          { "pattern" => 'db\/migrate.*.rb\z', "label" => "sql migration" }] } } }
   end
 
   before do
@@ -53,6 +54,36 @@ describe CommitMonitorHandlers::Branch::PathBasedLabeler do
 
   context "where there are no Gemfile changes" do
     let(:new_files) { ["some/other/file.rb"] }
+
+    it "does not add a label to the PR" do
+      expect(GithubService).to_not receive(:add_labels_to_an_issue)
+
+      described_class.new.perform(branch.id)
+    end
+  end
+
+  context "when there are migrations" do
+    let(:new_files) { ["db/migrate/20160706230546_some_migration.rb", "some/other/file.rb"] }
+
+    it "adds a label to the PR" do
+      expect(GithubService).to receive(:add_labels_to_an_issue).with(branch.repo.name, branch.pr_number, ["sql migration"])
+
+      described_class.new.perform(branch.id)
+    end
+  end
+
+  context "where there are no migrations" do
+    let(:new_files) { ["some/other/file.rb"] }
+
+    it "does not add a label to the PR" do
+      expect(GithubService).to_not receive(:add_labels_to_an_issue)
+
+      described_class.new.perform(branch.id)
+    end
+  end
+
+  context "where there are changes to migration specs only" do
+    let(:new_files) { ["spec/migrations/20140715200621_set_default_for_pxe_server_customization_directory_spec.rb", "some/other/file.rb"] }
 
     it "does not add a label to the PR" do
       expect(GithubService).to_not receive(:add_labels_to_an_issue)
