cd479c0843b06b8dffe1ccca7cf78925aaecc55f
Chris Arcand <chris@chrisarcand.com>
2017-02-28 00:19:30 -0600

    Fix broken Issue#set_milestone
    Who knows how long this thing has been broken, prolly the Github
    refactor...but apparently no one uses this as I haven't heard any
    complaints.
    
    API requires ID passing, not the name of the milestone.

--- lib/github_service/issue.rb
+++ lib/github_service/issue.rb
@@ -11,7 +11,12 @@ module GithubService
     end
 
     def set_milestone(milestone)
-      GithubService.update_issue(fq_repo_name, number, "milestone" => milestone)
+      if GithubService.valid_milestone?(fq_repo_name, milestone)
+        milestone_id = GithubService.milestones(fq_repo_name)[milestone]
+        GithubService.update_issue(fq_repo_name, number, "milestone" => milestone_id)
+      else
+        false
+      end
     end
 
     def add_comment(message)
