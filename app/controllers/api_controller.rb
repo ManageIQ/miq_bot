bd327d60801cbb55bd61338451062197f136839a
Julian Cheal <jcheal@redhat.com>
2017-05-02 13:30:40 -0500

    Initial commit of third party api integration.

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,9 @@
+class ApiController < ApplicationController
+  def index
+  end
+
+  private
+
+  def entrypoint_versions
+  end
+end
---
+++ spec/controllers/api_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ApiController, type: :controller do
+
+end
