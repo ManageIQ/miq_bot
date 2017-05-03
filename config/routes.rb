c9596e09eae63b9cbca52be4d63fa73bbe7c5077
Jason Frey <jfrey@redhat.com>
2017-05-03 12:14:46 -0400

    Remove Backport Requests feature
    The build manager uses a separate tool for handling this, which although
    it works in a similar way via GitHub search, is more scriptable and
    suited to the release process.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ MiqBot::Application.routes.draw do
 
   root 'main#index'
 
-  get '/backport_requests'                                => 'backport_requests#index'
-  get '/github_api_usage'                                 => 'github_api_usage#index'
+  get '/github_api_usage' => 'github_api_usage#index'
 end
