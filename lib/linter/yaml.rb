26c6e1a7d25e40b636120887e2dda39556cc807b
Brandon Dunne <bdunne@redhat.com>
2017-12-02 00:10:56 -0500

    Make line_to_hash method with some tests

--- lib/linter/yaml.rb
+++ lib/linter/yaml.rb
@@ -6,24 +6,14 @@ module Linter
 
     def parse_output(output)
       lines = output.chomp.split("\n")
-      parsed = lines.collect { |line| REGEX.match(line) }
-      grouped = parsed.group_by { |match| match[:filename] }
-      file_count = parsed.collect { |match| match[:filename] }.uniq.count
+      parsed = lines.collect { |line| line_to_hash(line) }
+      grouped = parsed.group_by { |hash| hash["filename"] }
+      file_count = parsed.collect { |hash| hash["filename"] }.uniq.count
       {
         "files"   => grouped.collect do |filename, offenses|
           {
             "path"     => filename.sub(%r{\A\./}, ""),
-            "offenses" => offenses.collect do |match|
-              {
-                "severity" => match[:severity],
-                "message"  => match[:message],
-                "cop_name" => match[:cop_name],
-                "location" => {
-                  "line"   => match[:line].to_i,
-                  "column" => match[:column].to_i
-                }
-              }
-            end
+            "offenses" => offenses.collect { |offense_hash| offense_hash.except("filename") }
           }
         end,
         "summary" => {
@@ -48,6 +38,20 @@ module Linter
 
     def filtered_files(files)
       files.select { |f| f.end_with?(".yml", ".yaml") }
+    end
+
+    def line_to_hash(line)
+      match = REGEX.match(line)
+      {
+        "filename" => match[:filename],
+        "severity" => match[:severity],
+        "message"  => match[:message],
+        "cop_name" => match[:cop_name],
+        "location" => {
+          "line"   => match[:line].to_i,
+          "column" => match[:column].to_i
+        }
+      }
     end
   end
 end
