b5c0c2bbba1f4cf96e329764b8196f133b80073b
Adrián Tóth <adtoth@redhat.com>
2018-02-28 10:52:10 +0100

    Removed unused variable "inspected_file_count"
    Variable "inspected_file_count" in the result of the linters has been removed due to missing use. It was not used in the pull request comment describing the offenses detected by linters in the source code.

--- spec/support/rubocop_spec_helper.rb
+++ spec/support/rubocop_spec_helper.rb
@@ -4,7 +4,7 @@ def rubocop_results
     rubocop = JSON.parse(`rubocop --format=json --no-display-cop-names #{rubocop_check_path}`)
     hamllint = JSON.parse(`haml-lint --reporter=json #{rubocop_check_path}`)
 
-    %w(offense_count target_file_count inspected_file_count).each do |m|
+    %w(offense_count target_file_count).each do |m|
       rubocop['summary'][m] += hamllint['summary'][m]
     end
     rubocop['files'] += hamllint['files']
