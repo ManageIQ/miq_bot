8ece59ac68aa75df7dfb60d7dbd1e0eeacf6892a
Adrián Tóth <adtoth@redhat.com>
2018-04-17 15:09:30 +0200

    Remove_reviewer implemented without dependence
    Command remove_reviewer is implemented by using the octokit's internal methods because the expected feature is not added yet. This is a temporary solution until merge of dependence in octokit.
    
    Closes #416

--- lib/github_service/commands/remove_reviewer.rb
+++ lib/github_service/commands/remove_reviewer.rb
@@ -12,8 +12,8 @@ module GithubService
             begin
               issue.remove_reviewer(user)
             rescue NoMethodError
-              issue.add_comment("@#{issuer} `remove_reviewer [@]user` is currently not working, waiting for merge"\
-                                " of [dependent pull request](https://github.com/octokit/octokit.rb/pull/990)!")
+              # TODO: Remove this exception handling after dependence merge.
+              octokit_request_pull_request_review(issue.fq_repo_name, issue.number, "reviewers" => [user])
             end
           else
             issue.add_comment("@#{issuer} '#{user}' is not in the list of requested reviewers, ignoring...")
@@ -26,6 +26,13 @@ module GithubService
       # returns an array of user logins who were requested for a pull request review
       def requested_reviewers
         GithubService.pull_request_review_requests(issue.fq_repo_name, issue.number).users.map(&:login)
+      end
+
+      # TODO: Remove this.
+      def octokit_request_pull_request_review(repo, id, reviewers, options = {})
+        service = GithubService.instance_variable_get("@service")
+        options = options.merge(:reviewers => reviewers.values.flatten)
+        service.delete("repos/#{repo}/pulls/#{id}/requested_reviewers", options)
       end
     end
   end
