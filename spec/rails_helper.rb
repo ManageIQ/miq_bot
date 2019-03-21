ce937deaf6b339af4d2388cf844f1fd8e9009ac0
Julian Cheal <jcheal@redhat.com>
2019-03-21 15:50:00 +0000

    Changed to new style Rails.root.join('path', 'to')

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,8 @@ require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
