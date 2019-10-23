742ce09ccf45f166791f12d153940efe99a326be
d-m-u <drewuhlmann@gmail.com>
2019-10-23 13:52:28 -0400

    Handle not having the run_tests_repo setting for the time being

--- config/initializers/ensure_run_tests_repo.rb
+++ config/initializers/ensure_run_tests_repo.rb
@@ -1,4 +1,6 @@
-if (url = Settings.run_tests_repo.url) && (name = Settings.run_tests_repo.name)
-  path = Repo::BASE_PATH.join(name)
-  MinigitService.clone(url, path) unless path.join(".git").exist?
+if Settings.run_tests_repo&.url
+  if (url = Settings.run_tests_repo.url) && (name = Settings.run_tests_repo.name)
+    path = Repo::BASE_PATH.join(name)
+    MinigitService.clone(url, path) unless path.join(".git").exist?
+  end
 end
