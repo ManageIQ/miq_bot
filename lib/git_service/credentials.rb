81efc3aa8e3223208d34820e28391ce5b8a0547e
Nick LaMuro <nicklamuro@gmail.com>
2020-05-18 13:15:55 -0500

    [GitService::Credentials] Add from_ssh_config
    Adds the `net-ssh` gem, which is used only to parse a SSH identity file
    from a ~/.ssh/confg (or equivalents), via `Net::SSH::Config.for`, to be
    used for git operations.
    
    GitService::Credentials.from_ssh_config uses that method to pull out a
    config for a given host, and will use the `User` field for that config
    if one isn't provided in the URL (which is the `username` arg in this
    class).

--- lib/git_service/credentials.rb
+++ lib/git_service/credentials.rb
@@ -1,11 +1,39 @@
 module GitService
   class Credentials
+    # Example:
+    #
+    #   GitService::Credentials.host_config = {
+    #     '*' => {
+    #       :username    => 'git',
+    #       :private_key => '~/.ssh/id_rsa'
+    #     },
+    #     'github.com' => {
+    #       :username    => 'git',
+    #       :private_key => '~/.ssh/id_rsa'
+    #     }
+    #   }
+    #
+    def self.host_config=(host_config = {})
+      @host_config = host_config.to_h
+    end
+
     # Generic method for finding git credentials based on what is available
     #
     # Will use ssh_agent if all other options have been exhausted.
     #
     def self.find_for_user_and_host(username, hostname)
-      from_ssh_agent(username)
+      from_ssh_config(username, hostname) || from_ssh_agent(username)
+    end
+
+    def self.from_ssh_config(username, hostname)
+      ssh_config = Net::SSH::Config.for(hostname)
+
+      return if ssh_config.empty? || ssh_config[:keys].nil?
+
+      ssh_config[:username]   = username || ssh_config[:user]             # favor URL username if present
+      ssh_config[:privatekey] = File.expand_path(ssh_config[:keys].first) # only use first key
+
+      Rugged::Credentials::SshKey.new(ssh_config)
     end
 
     def self.from_ssh_agent(username)
