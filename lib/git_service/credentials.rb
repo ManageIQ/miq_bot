0b149b1cc949c7ec631cfcbc439ad8fbcda50b80
Nick LaMuro <nicklamuro@gmail.com>
2020-05-18 19:28:36 -0500

    [GitService::Credentials] Add find_for_user_and_host
    This is a generic method for finding a set of credentials to use based
    on hostname and uri for a given git url (usually from a remote).
    
    Defaults to ssh-agent if no other options are available (using the
    `.ssh/config` via net-ssh will be added in a future commit)

--- lib/git_service/credentials.rb
+++ lib/git_service/credentials.rb
@@ -1,5 +1,13 @@
 module GitService
   class Credentials
+    # Generic method for finding git credentials based on what is available
+    #
+    # Will use ssh_agent if all other options have been exhausted.
+    #
+    def self.find_for_user_and_host(username, hostname)
+      from_ssh_agent(username)
+    end
+
     def self.from_ssh_agent(username)
       Rugged::Credentials::SshKeyFromAgent.new(:username => username)
     end
