5331ffcfefd6ed681d5c0dffd0cf9f5ecf5016e2
Nick LaMuro <nicklamuro@gmail.com>
2020-09-10 20:57:03 -0500

    [WIP] Use up to date linter configs
    Right now, what ever is on the "fetched master" for the linter configs
    will be what is used by Pronto, and not what is up to date with the
    branch being checked against.
    
    This "attempts" to generate a config based on the git blob for the
    branch we are checking, but it is still not quite working.

--- app/workers/concerns/code_analysis_mixin.rb
+++ app/workers/concerns/code_analysis_mixin.rb
@@ -5,6 +5,8 @@ require 'pronto/git/patches'
 require 'pronto/git/patch'
 require 'pronto/git/line'
 
+require 'tempfile'
+
 require 'fileutils'
 require 'tmpdir'
 
@@ -33,6 +35,7 @@ module CodeAnalysisMixin
     Pronto::GemNames.new.to_a.each { |gem_name| require "pronto/#{gem_name}" }
 
     branch.repo.git_fetch
+    generate_linter_configs
 
     git_service    = branch.git_service
     merge_base     = git_service.merge_base
@@ -40,6 +43,55 @@ module CodeAnalysisMixin
     pronto_patches = Pronto::Git::Patches.new(pronto_repo, merge_base, git_service.diff.raw_diff)
 
     Pronto::Runners.new.run(pronto_patches)
+  end
+
+  # configs in the repo itself most likely will be invalid as the what the
+  # `MinigitService` has checked out in `.repos/` probably isn't the same SHA
+  # as what we are working with.
+  #
+  # Any specifics per linter that need to be adjusted need to be done here, but
+  # RuboCop might be the only one that is necessary for now.
+  #
+  def generate_linter_configs
+    # Ensure any tempfiles created here are kept around through
+    # `.run_all_linters`, and will be unlinked there.
+    @config_tempfiles = []
+
+    # This is config option found in the `proto-rubocop` plugin README:
+    #
+    #   > You can also specify a custom .rubocop.yml location with the
+    #   > environment variable RUBOCOP_CONFIG.
+    #
+    git_service              = branch.git_service
+    rubocop_config_contents  = git_service.content_at(".rubocop.yml")
+    yamllint_config_contents = git_service.content_at(".yamllint")
+
+    if rubocop_config_contents
+      rubocop_yaml_data = YAML.load(rubocop_config_contents)
+      (rubocop_yaml_data.delete("inherit_from") || []).each do |local_config_path|
+        next unless inherit_from_data = git_service.content_at(local_config_path)
+
+        rubocop_yaml_data["inherit_from"] ||= []
+        rubocop_yaml_data["inherit_from"]  << generate_temp_config(local_config_path, inherit_from_data)
+      end
+
+      ENV["RUBOCOP_CONFIG"] = generate_temp_config(".rubocop.yml", rubocop_yaml_data.to_yaml)
+    end
+
+    if yamllint_config_contents
+      yamllint_config_path  = generate_temp_config(".yamllint", yamllint_config_contents)
+      ENV["YAMLLINT_OPTS"] = "-c #{yamllint_config_path}"
+    end
+  end
+
+  def generate_temp_config(config_filepath, file_contents)
+    temp_config = Tempfile.new(config_filepath)
+    temp_config.write file_contents
+    temp_config.close
+
+    @config_tempfiles << temp_config
+
+    temp_config.path
   end
 
   def run_all_linters
@@ -70,6 +122,8 @@ module CodeAnalysisMixin
     end
   rescue RuboCop::ValidationError => error
     [failed_linter_offenses("#{self.class.name} STDERR:\n```\n#{error.message}\n```")]
+  ensure
+    @config_tempfiles.each(&:unlink)
   end
 
   def failed_linter_offenses(message)
