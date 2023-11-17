2b0cea11fea63ad2422ce8bd6515e3804c97f916
Jason Frey <fryguy9@gmail.com>
2023-11-17 08:58:01 -0500

    Upgrade to Rails 6.0 / Ruby 2.7 via rake app:update

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.8', '>= 5.2.8.1'
+gem 'rails', '~> 6.0.0', '>= 6.0.6.1'
 
 # Use PostgreSQL as the database for Active Record
 gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require 'sidekiq/web'
 require 'sidecloq/web'
 
-MiqBot::Application.routes.draw do
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   mount Sidekiq::Web, at: "/sidekiq"
 
   root 'main#index'
