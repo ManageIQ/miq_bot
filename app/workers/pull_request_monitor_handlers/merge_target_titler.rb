98007e58b7a61af7be1a1a130002614284f32a6b
Jason Frey <fryguy9@gmail.com>
2023-11-20 14:10:51 -0500

    Fix Ruby 2.7 issue where module is not being recognized

--- app/workers/pull_request_monitor_handlers/merge_target_titler.rb
+++ app/workers/pull_request_monitor_handlers/merge_target_titler.rb
@@ -1,31 +1,33 @@
-class PullRequestMonitorHandlers::MergeTargetTitler
-  include Sidekiq::Worker
-  sidekiq_options :queue => :miq_bot
+module PullRequestMonitorHandlers
+  class MergeTargetTitler
+    include Sidekiq::Worker
+    sidekiq_options :queue => :miq_bot
 
-  include BranchWorkerMixin
+    include BranchWorkerMixin
 
-  def perform(branch_id)
-    return unless find_branch(branch_id, :pr)
+    def perform(branch_id)
+      return unless find_branch(branch_id, :pr)
 
-    process_branch
-  end
+      process_branch
+    end
 
-  private
+    private
 
-  def process_branch
-    apply_title if merge_target != "master" && !already_titled?
-  end
+    def process_branch
+      apply_title if merge_target != "master" && !already_titled?
+    end
 
-  def already_titled?
-    pr_title_tags.map(&:downcase).include?(merge_target.downcase)
-  end
+    def already_titled?
+      pr_title_tags.map(&:downcase).include?(merge_target.downcase)
+    end
 
-  def apply_title
-    logger.info("Updating PR #{pr_number} with title change for merge target #{merge_target}.")
-    GithubService.update_pull_request(fq_repo_name, pr_number, :title => new_pr_title)
-  end
+    def apply_title
+      logger.info("Updating PR #{pr_number} with title change for merge target #{merge_target}.")
+      GithubService.update_pull_request(fq_repo_name, pr_number, :title => new_pr_title)
+    end
 
-  def new_pr_title
-    "[#{merge_target.upcase}] #{pr_title}"
+    def new_pr_title
+      "[#{merge_target.upcase}] #{pr_title}"
+    end
   end
 end
--- app/workers/pull_request_monitor_handlers/wip_labeler.rb
+++ app/workers/pull_request_monitor_handlers/wip_labeler.rb
@@ -1,38 +1,40 @@
-class PullRequestMonitorHandlers::WipLabeler
-  include Sidekiq::Worker
-  sidekiq_options :queue => :miq_bot
+module PullRequestMonitorHandlers
+  class WipLabeler
+    include Sidekiq::Worker
+    sidekiq_options :queue => :miq_bot
 
-  include BranchWorkerMixin
+    include BranchWorkerMixin
 
-  LABEL = "wip".freeze
+    LABEL = "wip".freeze
 
-  def perform(branch_id)
-    return unless find_branch(branch_id, :pr)
+    def perform(branch_id)
+      return unless find_branch(branch_id, :pr)
 
-    process_branch
-  end
-
-  private
-
-  def process_branch
-    if wip_in_title?
-      apply_label
-    else
-      remove_label
+      process_branch
     end
-  end
 
-  def wip_in_title?
-    pr_title_tags.map(&:downcase).include?(LABEL)
-  end
+    private
 
-  def apply_label
-    logger.info("Updating PR #{pr_number} with label #{LABEL.inspect}.")
-    GithubService.add_labels_to_an_issue(fq_repo_name, pr_number, [LABEL])
-  end
+    def process_branch
+      if wip_in_title?
+        apply_label
+      else
+        remove_label
+      end
+    end
 
-  def remove_label
-    logger.info("Updating PR #{pr_number} without label #{LABEL.inspect}.")
-    GithubService.remove_label(fq_repo_name, pr_number, LABEL)
+    def wip_in_title?
+      pr_title_tags.map(&:downcase).include?(LABEL)
+    end
+
+    def apply_label
+      logger.info("Updating PR #{pr_number} with label #{LABEL.inspect}.")
+      GithubService.add_labels_to_an_issue(fq_repo_name, pr_number, [LABEL])
+    end
+
+    def remove_label
+      logger.info("Updating PR #{pr_number} without label #{LABEL.inspect}.")
+      GithubService.remove_label(fq_repo_name, pr_number, LABEL)
+    end
   end
 end
