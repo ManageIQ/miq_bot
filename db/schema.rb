8704f5ccbc6ef583a33898432e6e811d2767c9af
Brandon Dunne <brandon.dunne@ibm.com>
2025-03-05 18:06:01 -0500

    Add database level uniqueness on branch records
    Somehow we have duplicate (2-3) branch records being created at the same
    time (within 1 second) in the prod database and the rails validation is
    not good enough.  Let the database handle the uniqueness to prevent this
    from happening in the future.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_10_06_050814) do
+ActiveRecord::Schema.define(version: 2025_03_05_205548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2017_10_06_050814) do
     t.string "merge_target"
     t.string "pr_title"
     t.integer "linter_offense_count"
+    t.index ["name", "repo_id"], name: "index_branches_on_name_and_repo_id", unique: true
   end
 
   create_table "repos", id: :serial, force: :cascade do |t|
